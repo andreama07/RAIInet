8de13b728a76210a0d7bf8109deb445b9ab2dc62
DalyaTasli <74269835+DalyaTasli@users.noreply.github.com>
2023-11-27 22:45:47 -0500

    Update board.cc

--- board.cc
+++ board.cc
@@ -1,1 +1,14 @@
-//test
+module board;
+using namespace std;
+
+GameBoard::GameBoard() {}
+
+bool isWithinBounds(int x, int y) {
+    // need a size filed
+  if (x <= 8 && y <= 8) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
