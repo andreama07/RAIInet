82f3d3326e95e73fbe4cfc3040487779b4540b34
DalyaTasli <74269835+DalyaTasli@users.noreply.github.com>
2023-11-30 11:42:54 -0500

    I added a game control file to take in text to control the game after initialization in main

---
+++ gameControl.cc
@@ -0,0 +1,47 @@
+#include <gameControl.h>
+
+gameControl::gameControl() {}
+
+gameControl::~gameControl() {}
+
+    // put this in a function??
+
+  // interactions
+    while (true) {
+        cin >> command;
+        if (command == "move") {
+            // make sure the next is a calid link for the player
+            // implement
+            string pLink;
+            cin >> pLink;
+            string dir;
+            cin >> dir;
+            // move such link in said direction
+            // implement 
+            //redraw graph in text and graphics
+            // for the graphic redraw, redraw as little as possible
+        }
+        else if (command == "abilities") {
+            // display all abilities player has with an ID 1-5
+            // make an indication whether it was used or not
+        }
+        else if (command == "ability") {
+            int n;
+            cin >> n;
+            // uses ability with ID n 
+            // depending on ability, require further input
+        }
+        else if (command == "board") {
+            // display board
+        }
+        else if(command == "sequence") {
+            string f;
+            cin >> f;
+            // execute the sequence of commands found in file
+        }
+        // or if EOF
+        else if (command == "quit") {
+            // exit game
+        }
+    }
+}
---
+++ gameControl.h
@@ -0,0 +1,10 @@
+#ifndef __GAMECONTROL_H__
+#define __GAMECONTROL_H__
+
+class gameControl {
+public:
+    gameControl();
+    ~gameControl();
+};
+
+#endif
